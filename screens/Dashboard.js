6970158ffd38395993e844f69d8f9535fa3edc57
cherorafi <saidun2001@gmail.com>
2023-02-19 03:26:26 -0500

    registration working

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -1,12 +1,55 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { firebase } from '../config'
 
 const Dashboard = () => {
+  const [name, setName] = useState('')
+
+  useEffect(() => {
+    firebase.firestore().collection('users')
+    .doc(firebase.auth().currentUser.uid).get()
+    .then((snapshot) => {
+      if(snapshot.exists){
+        setName(snapshot.data())
+      }
+      else {
+        console.log('User does not exist')
+      }
+    })
+  }, [])
+
   return (
-    <View>
-      <Text>Dashboard</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={{fontSize: 20, fontWeight: 'bold'}}>
+        Hello, {name.firstName}
+      </Text>
+      <TouchableOpacity
+        onPress={() => {firebase.auth().signOut()}}
+        style={styles.button}
+      >
+        <Text style={{fontsize: 20, fontWeight: 'bold'}}>
+          Log Out
+        </Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   )
 }
 
 export default Dashboard
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 100,
+  },
+  button: {
+    marginTop: 50,
+    height: 70,
+    width: 250,
+    backgroundColor: '#026efd',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 50,
+  }
+})
--- screens/Registration.js
+++ screens/Registration.js
@@ -1,12 +1,114 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native'
+import React, {useState} from 'react'
+import { firebase } from '../config'
 
 const Registration = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+
+  registerUser = async (email, password, firstName, lastName) => {
+    await firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(() => {
+      firebase.auth().currentUser.sendEmailVerification({
+        handleCodeInApp: true,
+        url: 'https://book-buddy-f712f.firebaseapp.com'
+      })
+      .then(() => {
+        alert('Verification email sent')
+      }).catch((error) => {
+        alert(error.message)
+      })
+      .then(() => {
+        firebase.firestore().collection('users')
+        .doc(firebase.auth().currentUser.uid)
+        .set({
+          firstName,
+          lastName,
+          email,
+        })
+      })
+      .catch((error) => {
+        alert(error.message)
+      })
+    })
+    .catch((error => {
+      alert(error.message)
+    }))
+  }
+
   return (
-    <View>
-      <Text>Registration</Text>
+    <View style={styles.container}>
+      <Text style={{fontWeight: 'bold', fontSize: 23}}>
+        Sign Up
+      </Text>
+      <View style={{marginTop:40}}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="First Name"
+          onChangeText={(firstName) => setFirstName(firstName)}
+          autoCorrect={false}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Last Name"
+          onChangeText={(lastName) => setLastName(lastName)}
+          autoCorrect={false}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Email"
+          onChangeText={(email) => setEmail(email)}
+          autoCapitalize="none"
+          autoCorrect={false}
+          keyboardType="email-address"
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Password"
+          onChangeText={(password) => setPassword(password)}
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry={true}
+        />
+      </View>
+      <TouchableOpacity
+        onPress={() => registerUser(email, password, firstName, lastName)}
+        style={styles.button}
+      >
+        <Text style={{fontWeight: 'bold', fontSize: 22}}>Sign Up</Text>
+      </TouchableOpacity>
     </View>
   )
+
 }
 
 export default Registration
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 100,
+  },
+  textInput: {
+    paddingTop: 20,
+    paddingBottom: 10,
+    width: 400,
+    fontSize: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#000',
+    marginBottom: 10,
+    textAlign: 'center'
+  },
+  button: {
+    marginTop: 50,
+    height: 70,
+    width: 250,
+    backgroundColor: '#026efd',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 50,
+  }
+})
