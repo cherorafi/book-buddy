35393e1237f9263cb04ca68326aae3c84dbcbd48
cherorafi <saidun2001@gmail.com>
2023-03-04 02:35:56 -0500

    re-merge old functions

--- screens/Home.js
+++ screens/Home.js
@@ -1,21 +1,6 @@
 import { Text, StyleSheet, SafeAreaView } from 'react-native'
-import CreateList from '../components/MakeList'
-import ShowReadingLists from '../components/ShowReadingLists'
-import AddBook from '../components/AddBook'
-
-// Added unstyled functional components to make lists and show them
-// w/ real time changes
 
 const Home = () => {
-    /* for testing u can add in the view
-    <CreateList/>                // will display a button to create a new reading list
-
-    <ShowReadingLists/>          // will show a list of user's current reading lists
-
-    <AddBook bookName="Hamlet"/> // should be on a book's page, 
-                                 // will bring out a menuu of current lists
-                                 // to pick from, pass in name of book for now
-    */
     return (
         <SafeAreaView style={styles.container}>
         <Text style={{fontSize: 20, fontWeight: 'bold'}}>
