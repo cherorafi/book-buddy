5308c43891ef90617ed425eaf55cd4aa1123754e
unknown <41971154+buffalos@users.noreply.github.com>
2023-03-05 01:36:11 -0500

    added pop-up and firebase funcs

--- screens/BookView.js
+++ screens/BookView.js
@@ -1,7 +1,7 @@
-import { Text, StyleSheet, SafeAreaView, TouchableOpacity, View } from 'react-native'
+import { Text, StyleSheet, SafeAreaView, TouchableOpacity, View, Image } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
-import { Image } from 'react-native'
+import AddBook from '../components/AddBook.js'
 import { firebase } from '../config'
 import {examplecover} from '../assets/examplecover.jpg'
 
@@ -15,21 +15,23 @@ const BookView = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{height: '50%'}}>
-        <Image source={require('../assets/examplecover.jpg')} style={{width: 40, height: 90}}/>
-        <Text style={{fontSize: 20, fontWeight: 'bold', textAlignVertical: "top"}}>
+      <View style={{height: '50%', justifyContent: 'center', alignItems: 'center'}}>
+        <Image source={require('../assets/examplecover.jpg')} style={{width: 134, height: 213}}/>
+        <Text style={{fontSize: 20, fontWeight: 'bold', textAlignVertical: "top", textAlign: "center"}}>
           {title}
         </Text>
         <Text style={{fontSize: 15, textAlignVertical: "top"}}>{author}</Text>
+        <AddBook/>
       </View>
-      <TouchableOpacity
+      
+      {/* <TouchableOpacity
         onPress={() => navigation.navigate('AddToList')}
         style={styles.button}
       >
         <Text style={{fontsize: 20, fontWeight: 'bold'}}>
           AddToList
         </Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
     </SafeAreaView>
   )
 }
