2ec919766e1730320734b28f85b9ede4803f5f8c
unknown <41971154+buffalos@users.noreply.github.com>
2023-03-05 01:35:28 -0500

    changed size

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Header = (props) => {
   return (
     <View style={{marginLeft:15}}>
-      <Text style={{fontWeight:'bold', fontSize:28}}>
+      <Text style={{fontWeight:'bold', fontSize: 20, textAlignVertical: 'center'}}>
         {props.name}
       </Text>
     </View>
