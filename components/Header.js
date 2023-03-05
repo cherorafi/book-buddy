a0d2ab3c5c36b55e715ac8512415c8466caf2347
unknown <41971154+buffalos@users.noreply.github.com>
2023-03-05 14:27:14 -0500

    updated along with main

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Header = (props) => {
   return (
     <View style={{marginLeft:15}}>
-      <Text style={{fontWeight:'bold', fontSize: 20, textAlignVertical: 'center'}}>
+      <Text style={{fontWeight:'bold', fontSize:28}}>
         {props.name}
       </Text>
     </View>
