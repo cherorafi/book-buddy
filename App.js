e7fad9604acc352e56e918b61c27af4e799f5172
cherorafi <80270966+cherorafi@users.noreply.github.com>
2023-03-19 23:50:15 -0400

    Update App.js

--- App.js
+++ App.js
@@ -3,223 +3,200 @@ import { createStackNavigator } from "@react-navigation/stack";
 import React, { useState, useEffect } from 'react';
 import { firebase } from './config.js';
 
-
 import Login from "./screens/Login";
 import Registration from "./screens/Registration";
 import Home from "./screens/Home";
 import Chat from "./screens/Chat";
 import Search from "./screens/Search";
 import Profile from "./screens/Profile";
-import EditYourProfile from "./screens/EditYourProfile"
-import Settings from "./screens/Settings";
-
-
-
-
+import BookView from "./screens/BookView.js";
 import Header from "./components/Header";
-import WantToRead from "./screens/WantToRead.js";
-import Finished from "./screens/Finished.js";
+import BookListPage from "./screens/BookListPage.js";
+import Settings from "./screens/Settings.js";
+import EditYourProfile from "./screens/EditYourProfile"
 //import { Stack } from "@react-navigation/stack";
 
-
 const Stack = createStackNavigator();
-
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from 'react-native-vector-icons';
 
-
+/* // For demos only
+import { LogBox } from 'react-native';
+LogBox.ignoreAllLogs();//Ignore all log notifications
+*/
 
 const Tab = createBottomTabNavigator();
 
-
 const HomeStack = () => {
-   return (
-     <Stack.Navigator>
-       <Stack.Screen name="Home" Options={{ headerShown: false }} component={Home} />
-       <Stack.Screen name="WantToRead" component={WantToRead} />
-       <Stack.Screen name="Finished" component={Finished} />
-     </Stack.Navigator>
-   );
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="Home " Options={{ headerShown: false }} component={Home} />
+        <Stack.Screen name="BookList" component={BookListPage} />
+      </Stack.Navigator>
+    );
 };
-const UserProfile = () => {
-   return (
-     <Stack.Navigator>
-       <Stack.Screen name="Profile" Options={{ headerShown: false }} component={Profile} />
-       <Stack.Screen name="Edit Your Profile" component={EditYourProfile} />
-       <Stack.Screen name="Settings" component={Settings} />
 
-     </Stack.Navigator>
-   );
- };
- 
+
+const SearchStack = () => {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="Search " Options={{ headerShown: false }} component={Search} />
+        <Stack.Screen name="BookView" component={BookView} />
+      </Stack.Navigator>
+    );
+};
+
+const ProfileStack = () => {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="Profile " Options={{ headerShown: false }} component={Profile} />
+        <Stack.Screen name="Settings" component={Settings} />
+        <Stack.Screen name="Edit Your Profile" component={EditYourProfile} />
+
+      </Stack.Navigator>
+    );
+};
+
 function MyTabs() {
-   return (
-       <Tab.Navigator>
-           <Tab.Screen name="Home " component={HomeStack}
-               options={{
-                   tabBarIcon: ({ color, size }) => (
-                       <Ionicons name="ios-home" color={color} size={size} />
-                   ),
-                   headerTitle: () => <Header name="Book Buddy" />,
-                   headerStyle: {
-                       height: 150,
-                       borderBottomLeftRadius: 50,
-                       borderBottomRightRadius: 50,
-                       backgroundColor: '#00e4d0',
-                       shadowColor: '#000',
-                       elevation: 25
-                   }
-               }}
-           />
-           <Tab.Screen name="Chat" component={Chat}
-               options={{
-                   tabBarIcon: ({ color, size }) => (
-                       <Ionicons name="ios-chatbubble" color={color} size={size} />
-                   ),
-                   headerTitle: () => <Header name="Book Buddy" />,
-                   headerStyle: {
-                       height: 150,
-                       borderBottomLeftRadius: 50,
-                       borderBottomRightRadius: 50,
-                       backgroundColor: '#00e4d0',
-                       shadowColor: '#000',
-                       elevation: 25
-                   }
-               }}
-           />
-           <Tab.Screen name="Search" component={Search}
-               options={{
-                   tabBarIcon: ({ color, size }) => (
-                       <Ionicons name="ios-search" color={color} size={size} />
-                   ),
-                   headerTitle: () => <Header name="Book Buddy" />,
-                   headerStyle: {
-                       height: 150,
-                       borderBottomLeftRadius: 50,
-                       borderBottomRightRadius: 50,
-                       backgroundColor: '#00e4d0',
-                       shadowColor: '#000',
-                       elevation: 25
-                   }
-               }}
-           />
-           <Tab.Screen name="Profile " component={UserProfile}
-               options={{
-                   tabBarIcon: ({ color, size }) => (
-                       <Ionicons name="ios-person" color={color} size={size} />
-                   ),
-                   headerTitle: () => <Header name="Book Buddy" />,
-                   headerStyle: {
-                       height: 150,
-                       borderBottomLeftRadius: 50,
-                       borderBottomRightRadius: 50,
-                       backgroundColor: '#00e4d0',
-                       shadowColor: '#000',
-                       elevation: 25
-                   }
-               }}
-           />
-   </Tab.Navigator>
- );
-
-
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="HomeMain" component={HomeStack}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="ios-home" color={color} size={size} />
+                    ),
+                    headerTitle: () => <Header name="Book Buddy" />,
+                    headerStyle: {
+                        height: 35,
+                        borderBottomLeftRadius: 50,
+                        borderBottomRightRadius: 50,
+                        backgroundColor: '#111111',
+                        shadowColor: '#000',
+                        opacity: 0,
+                        elevation: 25
+                    }
+                }}
+            />
+            <Tab.Screen name="Chat" component={Chat} 
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="ios-chatbubble" color={color} size={size} />
+                    ),
+                    headerTitle: () => <Header name="Book Buddy" />,
+                    headerStyle: {
+                        height: 5,
+                        borderBottomLeftRadius: 50,
+                        borderBottomRightRadius: 50,
+                        backgroundColor: '#111111',
+                        opacity: 0,
+                        shadowColor: '#000',
+                        elevation: 25
+                    }
+                }} 
+            />
+            <Tab.Screen name="Search" component={SearchStack} 
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="ios-search" color={color} size={size} />
+                    ),
+                    headerTitle: () => <Header name="Book Buddy" />,
+                    headerStyle: {
+                        height: 20,
+                        borderBottomLeftRadius: 50,
+                        borderBottomRightRadius: 50,
+                        backgroundColor: '#111111',
+                        opacity: 0,
+                        shadowColor: '#000',
+                        elevation: 25
+                    }
+                }} 
+            />
+            <Tab.Screen name="Profile" component={ProfileStack} 
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="ios-person" color={color} size={size} />
+                    ),
+                    headerTitle: () => <Header name="Book Buddy" />,
+                    headerStyle: {
+                        height: 50,
+                        borderBottomLeftRadius: 50,
+                        borderBottomRightRadius: 50,
+                        backgroundColor: '#111111',
+                        opacity: 0,
+                        shadowColor: '#000',
+                        elevation: 25
+                    }
+                }} 
+            />
+        </Tab.Navigator>
+    );
 }
-
-
 
 
 function App() {
-   const [initializing, setInitializing] = useState(true);
-   const [user, setUser] = useState();
+    const [initializing, setInitializing] = useState(true);
+    const [user, setUser] = useState();
 
+    // user state changes
+    function onAuthStateChanged(user) {
+        setUser(user);
+        if (initializing) setInitializing(false);
+    }
 
-   // user state changes
-   function onAuthStateChanged(user) {
-       setUser(user);
-       if (initializing) setInitializing(false);
-   }
+    useEffect(() => {
+        const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
+        return subscriber;
+    }, []);
 
+    if (initializing) return null;
 
-   useEffect(() => {
-       const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
-       return subscriber;
-   }, []);
-
-
-   if (initializing) return null;
-
-
-   if (!user) {
-       return (
-           <Stack.Navigator>
-               <Stack.Screen
-                   name="Login"
-                   component={Login}
-                   options={{
-                       headerTitle: () => <Header name="Book Buddy" />,
-                       headerStyle: {
-                           height: 150,
-                           borderBottomLeftRadius: 50,
-                           borderBottomRightRadius: 50,
-                           backgroundColor: '#00e4d0',
-                           shadowColor: '#000',
-                           elevation: 25
-                       }
-                   }}
-               />
-               <Stack.Screen
-                   name="Registration"
-                   component={Registration}
-                   options={{
-                       headerTitle: () => <Header name="Book Buddy" />,
-                       headerStyle: {
-                           height: 150,
-                           borderBottomLeftRadius: 50,
-                           borderBottomRightRadius: 50,
-                           backgroundColor: '#00e4d0',
-                           shadowColor: '#000',
-                           elevation: 25
-                       }
-                   }}
-               />
-           </Stack.Navigator>
-       );
-   }
-   // else{
-   //     return (
-   //         <Stack.Navigator>
-   //             <Stack.Screen
-   //                 name="WantToRead"
-   //                 component={WantToRead}
-   //                 options={{
-   //                     headerTitle: () => <Header name="Book Buddy" />,
-   //                     headerStyle: {
-   //                         height: 150,
-   //                         borderBottomLeftRadius: 50,
-   //                         borderBottomRightRadius: 50,
-   //                         backgroundColor: '#00e4d0',
-   //                         shadowColor: '#000',
-   //                         elevation: 25
-   //                     }
-   //                 }}
-   //             />
-   //         </Stack.Navigator>
-   //     )
-   // }
-   return (
-       <MyTabs></MyTabs>
-       // <Stack.Navigator>
-       //     <MyTabs></MyTabs>
-       // </Stack.Navigator>
-   );
+    if (!user) {
+        return (
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Login"
+                    component={Login}
+                    options={{
+                        headerTitle: () => <Header name="Book Buddy" />,
+                        headerStyle: {
+                            height: 150,
+                            borderBottomLeftRadius: 50,
+                            borderBottomRightRadius: 50,
+                            backgroundColor: '#00e4d0',
+                            shadowColor: '#000',
+                            elevation: 25
+                        }
+                    }}
+                />
+                <Stack.Screen
+                    name="Registration"
+                    component={Registration}
+                    options={{
+                        headerTitle: () => <Header name="Book Buddy" />,
+                        headerStyle: {
+                            height: 150,
+                            borderBottomLeftRadius: 50,
+                            borderBottomRightRadius: 50,
+                            backgroundColor: '#00e4d0',
+                            shadowColor: '#000',
+                            elevation: 25
+                        }
+                    }}
+                />
+            </Stack.Navigator>
+        );
+    }
+    
+    return (
+        <MyTabs></MyTabs>
+    );
 }
 
-
 export default () => {
-   return (
-       <NavigationContainer>
-           <App></App>
-       </NavigationContainer>
-   )
+    return (
+        <NavigationContainer>
+            <App></App>
+        </NavigationContainer>
+    )
 }
