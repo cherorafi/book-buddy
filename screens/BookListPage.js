d14b05da5966f3cca220121067aa4d1766a03885
sumaiya1709 <sumaiya1709@gmail.com>
2023-04-16 16:09:11 -0400

    Dark Mode BooklistPage

--- screens/BookListPage.js
+++ screens/BookListPage.js
@@ -1,13 +1,16 @@
 import { Text, StyleSheet, SafeAreaView, View, FlatList } from 'react-native';
 import { GetBooks } from '../components/Firestore';
 import SingleBookCard from '../components/SingleBookCard';
+import { useContext } from 'react';
+import ColorSchemeContext from './../ColorSchemeContext';
 
 const BookListPage = ({ route }) => {
   const { listName } = route.params;
   let bookList = GetBooks(listName);
-
+  const { colorScheme } = useContext(ColorSchemeContext);
+  
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={[styles.container, { backgroundColor: colorScheme === 'dark' ? '#222' : '#f5f5f5' }]}>
       <FlatList
         data={bookList}
         keyExtractor={(item, index) => index.toString()}
