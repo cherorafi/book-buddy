bbd23f281bba97103a5820c5ed02066967cc6e4d
cherorafi <saidun2001@gmail.com>
2023-05-19 14:40:35 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,13 @@
+/* 
+A header component created towards the first few weeks of the App
+To experiment with props/creating new components
+Only used in Reigstration now
+*/
+
 import { View, Text } from 'react-native'
 import React from 'react'
 
+// Props passed down is the app name
 const Header = (props) => {
   return (
     <View style={{marginLeft:50}}>
